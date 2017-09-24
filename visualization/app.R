0d1972ec293448c4f7e9993d49a15d32fc70628d
Grant Nguyen <grant.nguyen@gmail.com>
2017-09-23 23:07:04 -0700

    Add points of interest, conditionally-toggled.

--- visualization/app.R
+++ visualization/app.R
@@ -14,35 +14,37 @@ library(htmltools)
 ## Import sidewalk observation data
 data_dir <- "~/Documents/Git/sea_sidewalks/input_data"
 
-sidewalk_observations <- fread(file.path(data_dir, "SidewalkObservations.csv"))
+sidewalk_observations <- fread(file.path(data_dir, "sidewalk_obs_cleaned.csv"))
 sidewalk_verifications <- fread(file.path(data_dir, "SidewalkVerifications.csv"))
-observation_metadata <- fread(file.path(data_dir, "sidewalk_obs_locmeta.csv"), select = c("objectid", "C_DISTRICT", "s_hood"))
-setnames(observation_metadata, c("objectid", "C_DISTRICT", "s_hood"), c("OBJECTID", "council_district", "neighborhood"))
-sidewalk_observations <- merge(sidewalk_observations, observation_metadata, by = "OBJECTID")
+poi_data <- fread(file.path(data_dir, "pois_with_locs.csv"))
 
-setnames(sidewalk_observations, c("X", "Y"), c("longitude", "latitude"))
+observation_metadata <- fread(file.path(data_dir, "sidewalk_obs_locmeta.csv"), select = c("objectid", "C_DISTRICT", "s_hood"))
+setnames(observation_metadata, c("C_DISTRICT", "s_hood"), c("council_district", "neighborhood"))
+sidewalk_observations <- merge(sidewalk_observations, observation_metadata, by = "objectid")
+
+setnames(sidewalk_observations, c("x", "y"), c("longitude", "latitude"))
 observation_map <- data.table(raw_name = c("SURFCOND", "HEIGHTDIFF", "OBSTRUCT", "XSLOPE", "OTHER"),
                               formatted_name = c("Surface conditions", "Height difference", "Obstruction", "Cross-slope", "Other"))
 
 ## Create formatted data labels (TODO: Put this in a separate file)
-sidewalk_observations[, formatted_label := paste0("ID: ", OBJECTID, "<br>", "Sidewalk ID: ", SIDEWALK_UNITID, "<br>", "Issue Type: ", OBSERV_TYPE, "<br>")]
-sidewalk_observations[OBSERV_TYPE == "SURFCOND", formatted_label := paste0(formatted_label,
-                                                                           "Surface Condition: ", SURFACE_CONDITION)]
-sidewalk_observations[OBSERV_TYPE == "HEIGHTDIFF", formatted_label := paste0(formatted_label,
-                                                                             "Height difference (inches): ", HEIGHT_DIFFERENCE, "<br>",
-                                                                             "Level difference type: ", LEVEL_DIFFERENCE_TYPE, "<br>",
-                                                                             "Isolated cross slope: ", ISOLATED_CROSS_SLOPE, "<br>",
-                                                                             "Failing Shim: ", FAILING_SHIM)]
-sidewalk_observations[OBSERV_TYPE == "OBSTRUCT", formatted_label := paste0(formatted_label,
-                                                                           "Obstruction type: ", OBSTRUCTION_TYPE, "<br>",
-                                                                           "Clearance impacted: ", CLEARANCE_IMPACTED)]
-sidewalk_observations[OBSERV_TYPE == "OBSTRUCT" & CLEARANCE_IMPACTED %in% c("HORIZONTAL", "BOTH"), formatted_label := paste0(formatted_label,
+sidewalk_observations[, formatted_label := paste0("ID: ", objectid, "<br>", "Sidewalk ID: ", sidewalk_unitid, "<br>", "Issue Type: ", observ_type, "<br>")]
+sidewalk_observations[observ_type == "SURFCOND", formatted_label := paste0(formatted_label,
+                                                                           "Surface Condition: ", surface_condition)]
+sidewalk_observations[observ_type == "HEIGHTDIFF", formatted_label := paste0(formatted_label,
+                                                                             "Height difference (inches): ", height_difference, "<br>",
+                                                                             "Level difference type: ", level_difference_type, "<br>",
+                                                                             "Isolated cross slope: ", isolated_cross_slope, "<br>",
+                                                                             "Failing Shim: ", failing_shim)]
+sidewalk_observations[observ_type == "OBSTRUCT", formatted_label := paste0(formatted_label,
+                                                                           "Obstruction type: ", obstruction_type, "<br>",
+                                                                           "Clearance impacted: ", clearance_impacted)]
+sidewalk_observations[observ_type == "OBSTRUCT" & clearance_impacted %in% c("HORIZONTAL", "BOTH"), formatted_label := paste0(formatted_label,
                                                                                                                              "<br>",
-                                                                                                                             "Width at narrowest point of obstruction: ", MINIMUM_WIDTH)]
-sidewalk_observations[OBSERV_TYPE == "XSLOPE", formatted_label := paste0(formatted_label,
-                                                                         "Isolated cross slope: ", ISOLATED_CROSS_SLOPE)]
-sidewalk_observations[OBSERV_TYPE == "OTHER", formatted_label := paste0(formatted_label,
-                                                                        "Sidewalk Feature: ", OTHER_FEATURE)]
+                                                                                                                             "Width at narrowest point of obstruction: ", minimum_width)]
+sidewalk_observations[observ_type == "XSLOPE", formatted_label := paste0(formatted_label,
+                                                                         "Isolated cross slope: ", isolated_cross_slope)]
+sidewalk_observations[observ_type == "OTHER", formatted_label := paste0(formatted_label,
+                                                                        "Sidewalk Feature: ", other_feature)]
 
 ## Source all of the visualization code
 source("main_map.R")
@@ -58,7 +60,8 @@ ui <- function(request) {
     theme = shinytheme("paper"),
     tabPanel("Main Map", main_map_ui("main_map", 
                                      sidewalk_observations = sidewalk_observations,
-                                     observation_map = observation_map))
+                                     observation_map = observation_map,
+                                     poi_data = poi_data))
     # tabPanel("Report Card", report_card_ui("report_card", 
     #                                  sidewalk_observations = sidewalk_observations,
     #                                  observation_map = observation_map))
@@ -69,7 +72,8 @@ ui <- function(request) {
 server <- function(input,output,session) {
   callModule(main_map_server, "main_map",
              sidewalk_observations = sidewalk_observations,
-             observation_map = observation_map)
+             observation_map = observation_map,
+             poi_data = poi_data)
   # callModule(report_card_server, "report_card",
   #            sidewalk_observations = sidewalk_observations,
   #            observation_map = observation_map)
--- visualization/main_map.R
+++ visualization/main_map.R
@@ -2,7 +2,7 @@
 ## Shiny App to display an interactive map with all marked sidewalk observations and verifications
 
 ## Create the UI
-main_map_ui <- function(id, sidewalk_observations, observation_map) { 
+main_map_ui <- function(id, sidewalk_observations, observation_map, poi_data) { 
   ## Initialize namespace for the UI to connect with the server
   ns <- NS(id)
   
@@ -24,6 +24,7 @@ main_map_ui <- function(id, sidewalk_observations, observation_map) {
                                     "Number of Top Priority Issues",
                                     min = 1, max = 500, value = 100)
       ),
+      checkboxInput(ns("toggle_pois"), "Display Points of Interest (POIs)", FALSE),
       downloadButton(ns("download_data"),"Download Table")
     ),
     column(10,
@@ -43,7 +44,7 @@ main_map_ui <- function(id, sidewalk_observations, observation_map) {
 ## Set the Server for the visualization
 
 main_map_server <- function(input, output, session, 
-                            sidewalk_observations, observation_map) {
+                            sidewalk_observations, observation_map, poi_data) {
   
   ###################################################################
   ## Pull current and comparison results, and combine into a dataset
@@ -52,9 +53,8 @@ main_map_server <- function(input, output, session,
     if(input$sel_neighborhood != "All") observation_subset <- observation_subset[neighborhood == input$sel_neighborhood]
     if(input$sel_issue_type != "All") {
       selected_issue_type <- observation_map[formatted_name == input$sel_issue_type, raw_name]
-      observation_subset <- observation_subset[OBSERV_TYPE == selected_issue_type,]
+      observation_subset <- observation_subset[observ_type == selected_issue_type,]
     }
-
     observation_subset[, Priority := runif(nrow(observation_subset), 1, 5)]
     setorder(observation_subset, -Priority)
 
@@ -70,23 +70,48 @@ main_map_server <- function(input, output, session,
     return(top_point_geo_subset)
   })
 
+  poi_icon_values <- reactiveValues()
+  
+  poi_geo_subset <- reactive({
+    poi_geo_subset <- copy(poi_data)
+    if(input$sel_neighborhood != "All") poi_geo_subset <- poi_geo_subset[s_hood == input$sel_neighborhood]
+    
+    poi_icons <- data.table(poi_type = poi_geo_subset$Type)
+    poi_icons[poi_type %in% c("Health Centers - Community", "Health Centers - Public"), icon := "fa-hospital-o"]
+    poi_icons[poi_type %in% c("Elementary Schools", "High Schools", "Higher Education"), icon := "fa-graduation-cap"]
+    poi_icons[poi_type %in% c("Ferry Terminal", "Monorail", "Light Rail"), icon := "fa-bus"]
+    poi_icons[poi_type %in% c("Community Centers", "Farmers Markets", "Family Support Center", "Food Banks", "Neighborhood Service Centers"), icon := "fa-users"]
+    poi_icons[poi_type %in% c("Museums and Galleries", "Police Precincts", "General Attractions"), icon := "fa-star"]
+    
+    leafIcons <- awesomeIcons(
+      icon = poi_icons$icon,
+      library = "fa"
+    )
+    
+    poi_icon_values$value <- leafIcons
+    
+    return(poi_geo_subset)
+  })
 
   
   ###################################################################
   ## Generate a main map of 
   output$main_map <- leaflet::renderLeaflet({
     if(input$toggle_top_priority == FALSE) {
-      leaflet() %>%
-        addTiles() %>%
-        addMarkers(data = top_point_geo_subset(),
-                   clusterOptions = markerClusterOptions(),
-                   popup = ~formatted_label)
+      leaf_plot <- leaflet() %>%
+                    addTiles() %>%
+                    addMarkers(data = top_point_geo_subset(),
+                               clusterOptions = markerClusterOptions(),
+                               popup = ~formatted_label)
+      if(input$toggle_pois == TRUE) leaf_plot <- addAwesomeMarkers(leaf_plot, data = poi_geo_subset(), icon = poi_icon_values$value)
     } else {
-        leaflet() %>%
-        addTiles() %>%
-        addMarkers(data = top_point_geo_subset(),
-                   popup = ~formatted_label)
+      leaf_plot <- leaflet() %>%
+                  addTiles() %>%
+                  addMarkers(data = top_point_geo_subset(),
+                             popup = ~formatted_label)
+      if(input$toggle_pois == TRUE) leaf_plot <- addAwesomeMarkers(leaf_plot, data = poi_geo_subset(), icon = poi_icon_values$value)
     }
+    leaf_plot
   })
 
 
